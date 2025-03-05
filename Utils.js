812ccdb1bcebb47188076ceaf8c22e2ad82d1d75
Aman Sheikh <haarishsheikh04@gmail.com>
2025-03-05 21:28:58 +0530

    a small change

--- Utils.js
+++ Utils.js
@@ -1,7 +1,7 @@
 import { redirect } from "react-router-dom";
 
 export async function requireAuth() {
-    console.log("balle balle ho gaya mera...");
+    console.log("It's working...");
     return redirect("/login");
 }
 
